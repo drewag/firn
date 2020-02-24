eed758edf4c695b992d34571a4b4acfeedf6d274
Andrew Wagner <845683+drewag@users.noreply.github.com>
2020-02-24 14:38:40 -0700

    [CI] Add some comments to the example server

--- Sources/ExampleServer/main.swift
+++ Sources/ExampleServer/main.swift
@@ -3,6 +3,8 @@ import Firn
 let api = API()
 
 api.configureAuthentication(for: User.self) { request in
+    // An authorization configuration must return a user if the auth is valid.
+    // If it returns nil, the API will return an unauthorized response.
     guard request.headers["Authorization"].contains("Bearer SECURE") else {
         return nil
     }
@@ -10,17 +12,24 @@ api.configureAuthentication(for: User.self) { request in
 }
 
 try api.addRoutes {
+    // Returns an empty 200 status
     GET("ok")
 
+    // Returns a gone status
     GET("gone")
         .status(.gone)
 
+    // Returns "Hello World!"
     GET("hello")
         .toText({ _ in "Hello World!"})
 
+    // Returns whatever data is passed in
     POST("echo")
 
     Auth(User.self) {
+        // Returns a message confirming authentication as long as it
+        // includes the "Authorization: Bearer SECURE" header as defined
+        // in the auth configuration above
         GET("restricted")
             .toText({ req in
                 let user = try req.authorizedUser(User.self)
@@ -29,16 +38,21 @@ try api.addRoutes {
     }
 
     Auth(User.self) {
+        // Defines a group of endpoints all at "/tasks"
         Group("tasks") {
+            // Return a json representation of a Task
             GET("new")
                 .toObject({ _ in Task(name: "") })
 
+            // Parses and returns an json representation of a Task
             POST()
                 .toObject(Task.self)
 
+            // Returns a Task with the id in the path e.g. 3 for "/tasks/3"
             GET(Var.int)
                 .toObject({ Task(id: try $0.pathParams.int(at: 0), name: "Some task", isComplete: false) })
 
+            // Parses and returns an json representation of a Task
             PUT(Var.int)
                 .toObject(Task.self)
         }
